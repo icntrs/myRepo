0f9735e0901e8ce475c616121d2ff927165eab67
icntrs <electagls@hotmail.com>
2022-07-13 23:01:11 -0700

    add integrating test using mockito, junit spring framework test

--- billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-19T17:41:52+0100",
+    date = "2021-03-07T14:54:33+0100",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 1.8.0_212 (Oracle Corporation)"
 )
 @Component
